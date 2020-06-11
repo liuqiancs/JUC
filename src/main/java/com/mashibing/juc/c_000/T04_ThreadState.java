a0181a03a278684776ad24420e49d1542ed026ac
liuqian <liuq@linkstec.com>
2020-06-11 15:56:25 +0800

    完善线程状态测试类，新增死锁测试类

--- src/main/java/com/mashibing/juc/c_000/T04_ThreadState.java
+++ src/main/java/com/mashibing/juc/c_000/T04_ThreadState.java
@@ -1,38 +1,71 @@
 package com.mashibing.juc.c_000;
 
 public class T04_ThreadState {
-
-    static class MyThread extends Thread {
-        @Override
-        public void run() {
-            System.out.println(this.getState());
-
-            for(int i=0; i<10; i++) {
+    public static void main(String[] args) {
+        Object lockObj = new Object();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " state " +Thread.currentThread().getState());
+            }
+        },"t1");
+        System.out.println(t1.getName() + " state " +t1.getState());
+        t1.start();
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-                System.out.println(i);
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        Thread t = new MyThread();
-
-        System.out.println(t.getState());
-
-        t.start();
-
+        },"t2");
+        t2.start();
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this){
+                        this.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"t3");
+        t3.start();
+        Thread t4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (lockObj){
+                        Thread.sleep(2000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"t4");
+        t4.start();
+        Thread t5 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockObj){
+                }
+            }
+        },"t5");
+        t5.start();
         try {
-            t.join();
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        System.out.println(t.getState());
+        System.out.println(t1.getName() + " state " +t1.getState());
+        System.out.println(t2.getName() + " state " +t2.getState());
+        System.out.println(t3.getName() + " state " +t3.getState());
+        System.out.println(t4.getName() + " state " +t4.getState());
+        System.out.println(t5.getName() + " state " +t5.getState());
 
     }
 }
